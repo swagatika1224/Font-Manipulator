13e47be4b1a954258591bf570e076d042d502c12
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-10-29 19:42:50 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
 video = createCapture(VIDEO);
 video.size(550, 500);
@@ -11,6 +14,9 @@ poseNet.on('pose', gotPoses);
 
 function draw() {
 background('#ADD8E6');
+textSize(difference);
+fill('#FFE787');
+text('Peter', 50, 400);
 }
 
 function modelLoaded() {
@@ -20,5 +26,11 @@ console.log('PoseNet Is Initialized!');
 function gotPoses(results) {
 if(results.length > 0) {
 console.log(results);
+
+leftWristX = results[0].pose.leftWrist.x;
+rightWristX = results[0].pose.rightWrist.x;
+difference = floor(leftWWristX - rightWristX);
+
+console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX);
  }
 }
